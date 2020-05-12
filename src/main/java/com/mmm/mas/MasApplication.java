888744845ac3eed4cf3d2f556d4150f171bf611e
troublemove <1461504402@qq.com>
2020-05-12 20:11:32 +0800

    banner

--- src/main/java/com/mmm/mas/MasApplication.java
+++ src/main/java/com/mmm/mas/MasApplication.java
@@ -3,13 +3,20 @@ package com.mmm.mas;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.mmm.mas.dao")
-public class MasApplication {
+public class MasApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MasApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MasApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MasApplication.class);
+	}
 
 }
